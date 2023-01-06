39df7d7091432b421f35b648446e075b99a26b2c
Dean Fletcher <maxfletch101@yahoo.com>
2023-01-06 14:52:52 -0500

    Added update functionality to mogal

--- src/App.js
+++ src/App.js
@@ -1,42 +1,58 @@
 import "./index.css";
 import Employee from "./components/Employee";
 import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [role, setRole] = useState("dev");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Dean Fletcher",
       role: "Legend",
       img: "https://i.imgur.com/GCBVgXDb.jpg",
     },
     {
+      id: 2,
       name: "Danny Dogs",
       role: "Manager",
       img: "https://i.imgur.com/TFMBQb.jpg",
     },
     {
+      id: 3,
       name: "Grinch",
       role: "Intern",
       img: "https://i.imgur.com/uqExBdTb.jpg",
     },
     {
+      id: 4,
       name: "Sean Cannon",
       role: "Intern",
       img: "https://i.imgur.com/Jvh1OQmb.jpg",
     },
     {
+      id: 5,
       name: "Jelly Bean",
       role: "Developer",
       img: "https://i.imgur.com/C5lQTKXb.jpg",
     },
     {
+      id: 6,
       name: "JoJo",
       role: "Developer",
       img: "https://i.imgur.com/jw4f2POb.jpg",
     },
   ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
   const showEmployees = true;
   return (
     <div className='App'>
@@ -53,10 +69,12 @@ function App() {
             {employees.map((employee) => {
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -3,9 +3,9 @@ import EditEmployee from "./EditEmployee";
 function Employee(props) {
     return (
         <div className="py-8 px-8 m-2 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img 
-                className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" 
-                src={props.img} 
+            <img
+                className="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"
+                src={props.img}
                 alt="Employee's Face"
             />
             <div className="text-center space-y-2 sm:text-left">
@@ -14,7 +14,12 @@ function Employee(props) {
                     <p className="text-slate-500 font-medium">{props.role}</p>
                 </div>
 
-                <EditEmployee />
+                <EditEmployee
+                    id={props.id}
+                    name={props.name}
+                    role={props.role}
+                    updateEmployee={props.updateEmployee}
+                />
 
             </div>
         </div>
