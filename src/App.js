acaab44c3bb1866d0d186192552b7acfae26cb80
Dean Fletcher <maxfletch101@yahoo.com>
2023-01-06 09:55:26 -0500

    Moving components into data structure

--- src/App.js
+++ src/App.js
@@ -3,22 +3,38 @@ import Employee from './components/Employee';
 import { useState } from 'react';
 
 function App() {
-  const [role, setRole] = useState();
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
       {showEmployees ? (
         <>
-          <input type="text" onChange={(e) => {
-            console.log(e.target.value);
-            setRole(e.target.value)
-          }}
+          <input 
+            type="text" 
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value)
+            }}
           />
-          <Employee name="Dean" role="Legend" />
-          <Employee name="Abby" role={role} />
-          <Employee name="James"/>
+          <div className="flex flex-wrap justify-center">
+            <Employee 
+              name="Dean" 
+              role={role} 
+              img="https://imgur.com/gallery/GCBVgXD"
+            />
+            <Employee 
+              name="Dean" 
+              role={role} 
+              img="https://imgur.com/gallery/GCBVgXD"
+            />
+            <Employee 
+              name="Dean" 
+              role={role} 
+              img="https://imgur.com/gallery/GCBVgXD"
+            />
+          </div>
         </>
-      ):(  
+      ) : (  
         <p>You don't have proper permissions</p>
       )}
     </div>
