9d2794b56ae176484554281f9c8d55f1a1df1d89
Dean Fletcher <maxfletch101@yahoo.com>
2023-01-09 09:37:20 -0500

    changed for => forHTML in EditEmployee.js line 43

--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -40,7 +40,7 @@ function EditEmployee(props) {
                             <div className="md:w-1/3">
                                 <label
                                     className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
-                                    for="name"
+                                    forHTML="name"
                                 >
                                     Full Name
                                 </label>
