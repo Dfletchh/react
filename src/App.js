193dea7cb65ef0355a6bd3edf273d0a8eb027696
Dean Fletcher <maxfletch101@yahoo.com>
2023-01-09 11:18:30 -0500

    Navbar added

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { v4 as uuidv4 } from "uuid";
 import Employee from "./components/Employee";
 import AddEmployee from "./components/AddEmployee";
 import EditEmployee from "./components/EditEmployee";
+import Header from "./components/Header";
 
 function App() {
-  const [role, setRole] = useState("dev");
+  // const [role, setRole] = useState("dev");
   const [employees, setEmployees] = useState([
     {
       id: 1,
@@ -68,18 +69,13 @@ function App() {
   }
 
   const showEmployees = true;
+
   return (
-    <div className='App'>
+    <div className='App bg-gray-200 min-h-screen'>
+      <Header />
       {showEmployees ? (
         <>
-          <input
-            type='text'
-            onChange={(e) => {
-              console.log(e.target.value);
-              setRole(e.target.value);
-            }}
-          />
-          <div className='flex flex-wrap justify-center'>
+          <div className='flex flex-wrap justify-center my-2'>
             {employees.map((employee) => {
               const editEmployee = (
                 <EditEmployee
