1d45e745362b860a1934f4fe14fa9bd7549a58ed
Dean Fletcher <maxfletch101@yahoo.com>
2023-01-10 11:17:02 -0500

    try/catch for error codes

--- src/pages/Definition.js
+++ src/pages/Definition.js
@@ -6,6 +6,7 @@ import NotFound from '../components/404';
 export default function Definition() {
     const [word, setWord] = useState();
     const [notFound, setNotFound] = useState(false);
+    const [error, setError] = useState(false);
     let { search } = useParams();
     const navigate = useNavigate();
 
@@ -15,10 +16,18 @@ export default function Definition() {
                 if (response.status === 404) {
                     setNotFound(true);
                 }
+                if (!response.ok) {
+                    setError(true);
+
+                    throw new Error('Something went wront');
+                }
                 return response.json()
             })
             .then((data) => {
                 setWord(data[0].meanings)
+            })
+            .catch((e) => {
+                console.log(e.message);
             });
     }, []);
 
@@ -26,6 +35,14 @@ export default function Definition() {
         return (
             <>
                 <NotFound />
+                <Link to='/dictionary'>Search agian</Link>
+            </>
+        );
+    }
+    if (error === true) {
+        return (
+            <>
+                <p>Error: somthing went wrong</p>
                 <Link to='/dictionary'>Search agian</Link>
             </>
         );
